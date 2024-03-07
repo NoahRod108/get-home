6e813dc866ca608b2ae66e03f0daf44b70596b72
NoahRod108 <112348397+NoahRod108@users.noreply.github.com>
2024-03-06 23:48:49 -0600

    Added edit form

--- components/EditPropertyForm.jsx
+++ components/EditPropertyForm.jsx
@@ -566,7 +566,7 @@ const EditPropertyForm = () => {
 
         <div>
           <button
-            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+            className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
             type="submit"
           >
             Update Property
